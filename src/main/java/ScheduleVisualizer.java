82e41fb18413c6cf8251829dec0da983dd27fc22
ddealsan <ddealsan@myuax.com>
2024-02-21 12:24:26 +0100

    nueva clase

--- src/main/java/ScheduleVisualizer.java
+++ src/main/java/ScheduleVisualizer.java
@@ -1,2 +1,11 @@
+import java.util.Map;
+
 public class ScheduleVisualizer {
+    public void displaySchedule(Map<String, String> taskSchedule) {
+        // Visualizar los horarios y las cargas de trabajo
+        System.out.println("Horario de tareas:");
+        for (Map.Entry<String, String> entry : taskSchedule.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
 }
