5046345c8e75e441dc3f38c72ef577eeaa496c69
ddealsan <ddealsan@myuax.com>
2024-02-21 11:47:29 +0100

    creamos una clase

---
+++ src/main/java/TimeConverter.java
@@ -0,0 +1,11 @@
+public class TimeConverter {
+    public double convertEarthToPlanetTime(double earthSeconds) {
+        // Implementación de la conversión de tiempo de la Tierra al nuevo planeta
+        return earthSeconds * 1.5; // Ejemplo simple de conversión
+    }
+
+    public double convertPlanetToEarthTime(double planetSeconds) {
+        // Implementación de la conversión de tiempo del nuevo planeta a la Tierra
+        return planetSeconds / 1.5; // Ejemplo simple de conversión
+    }
+}
